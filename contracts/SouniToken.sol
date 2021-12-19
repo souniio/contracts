10b540893836aacdb7d73f1816edcffadcbed7cb
souni-dev <95521134+souni-dev@users.noreply.github.com>
2021-12-19 15:10:44 +0800

    optimize the code

--- contracts/SouniToken.sol
+++ contracts/SouniToken.sol
@@ -1,19 +1,26 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.10;
 
 
 import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetFixedSupply.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract SonTicket is Ownable, ERC20PresetFixedSupply {
+contract SouniToken is Ownable, ERC20PresetFixedSupply {
+
+    event SetWhiteList(address[] addresses, bool flag);
+    event SetLimitTime(uint256 indexed limitTime);
+    event SetOpenTime(uint256 indexed openTime);
+    event SetLimitAmount(uint256 indexed limitAmount);
 
     uint256 public limitTime = 1642251600;
     uint256 public openTime = 1642253400;
-    uint256 public limitAmount = 10000 * 10 ** 18;
+    uint256 public limitAmount = 1000000 * 10 ** 18;
 
     mapping(address => bool) private _whitelists;
 
-    constructor() ERC20PresetFixedSupply('Souni Token', 'SON', 10000000000 * 10 ** 18, msg.sender){}
+    constructor(address multiSigAddress) ERC20PresetFixedSupply('Souni Token', 'SON', 10000000000 * 10 ** 18, multiSigAddress){
+        transferOwnership(multiSigAddress);
+    }
 
     function _beforeTokenTransfer(address from_, address to_, uint256 amount_) internal override {
         if (block.timestamp < limitTime) {
@@ -27,21 +34,27 @@ contract SonTicket is Ownable, ERC20PresetFixedSupply {
         super._beforeTokenTransfer(from_, to_, amount_);
     }
 
-    function addWhiteList(address address_) external onlyOwner {
-        _whitelists[address_] = true;
+    function setWhiteList(address[] calldata addresses_, bool flag) external onlyOwner {
+        for (uint16 i = 0; i < addresses_.length; i ++) {
+            _whitelists[addresses_[i]] = flag;
+        }
+        emit SetWhiteList(addresses_, flag);
     }
 
     function setLimitTime(uint256 limitTime_) external onlyOwner {
         require(limitTime_ < openTime && limitTime_ < 1646092800, "invalid time");
         limitTime = limitTime_;
+        emit SetLimitTime(limitTime_);
     }
 
     function setOpenTime(uint256 openTime_) external onlyOwner {
         require(openTime_ > limitTime && openTime_ < 1646092800, "invalid time");
         openTime = openTime_;
+        emit SetOpenTime(openTime_);
     }
 
     function setLimitAmount(uint256 limitAmount_) external onlyOwner {
         limitAmount = limitAmount_;
+        emit SetLimitAmount(limitAmount_);
     }
 }
